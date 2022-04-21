8478bff42f72adfe1cf0859549ddc59607ff8fe8
Nguyen Van Quy <70767468+nguyenvanquy222@users.noreply.github.com>
2022-04-21 08:24:37 +0700

    sample1
    đã chia nhỏ components

---
+++ conponents/Sample4.js
@@ -0,0 +1,8 @@
+const Sample4 = () => {
+    return (      
+        <div class="duoi">
+        <p>Copyright © 2021 - danhgiananglucthpt.edu.vn</p>
+    </div>
+        );
+    };
+    export default Sample4;
---
+++ conponents/course.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+const Course = ({ name, link }) => {
+  return (
+    <div className="col-sm-3">
+      <div className="place-list">
+        <div className="place-body">
+          <img
+            src={`${process.env.PROXY_TARGET_URL}/${link}`}
+            width="300"
+            height="165"
+            className="place-img"
+          />
+          <a href="#" className="text">
+            {name}
+          </a>
+          <br />
+          <Link href="/learn/ailearn"><a
+            className="read-more text-white"
+          >
+            ReadMore >>
+          </a></Link>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Course;
