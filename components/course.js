d95d18717f1a227220a5194a9a12c927a506ecb4
nguyenhuuloc13 <76745593+nguyenhuuloc13@users.noreply.github.com>
2022-04-21 08:25:31 +0700

    thay đổi components

--- components/course.js
+++ components/course.js
@@ -1,29 +1,19 @@
 import Link from "next/link";
-import AIlearn from '../pages/learn/ailearn'
-const Course = ({ name, link }) => {
+
+const Course = ({name, link}) => {
   return (
+      
     <div className="col-sm-3">
       <div className="place-list">
         <div className="place-body">
-          <img
-            src={`${process.env.PROXY_TARGET_URL}/${link}`}
-            width="300"
-            height="165"
-            className="place-img"
-          />
-          <a href="#" className="text">
-            {name}
-          </a>
+          <img src={`${process.env.PROXY_TARGET_URL}/${link}`} width="300" height="165" className="place-img"/>
+          <a href="dangnhap.html" className="text">{name} </a>
           <br />
-          <Link href="/learn/ailearn"><a
-            className="read-more text-white"
-          >
-            ReadMore >>
-          </a></Link>
-          
+          <a onClick={() => ReadMore()} href="dangnhap.html" className="read-more text-white"> Xem Thêm </a>
         </div>
       </div>
     </div>
   );
 };
+
 export default Course;
---
+++ components/duoi.js
@@ -0,0 +1,12 @@
+const Duoi = ({name, link}) => {
+    return (
+        <div className="footer bg-white">
+        <div class="duoi">
+        <p>Copyright © 2021 - danhgiananglucthpt.edu.vn</p>
+    </div>
+    
+</div>
+    );
+  };
+  
+  export default Duoi;
